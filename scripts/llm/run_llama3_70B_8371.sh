7ddf0a5bcb76bc2897ccc810e3b42c8abeb9a6e7
2016bits <1835154145@qq.com>
2026-03-09 16:06:43 +0800

    first commit

---
+++ scripts/llm/run_llama3_70B_8371.sh
@@ -0,0 +1,10 @@
+export CUDA_VISIBLE_DEVICES=1
+python -O -u -m vllm.entrypoints.openai.api_server \
+        --host=127.0.0.1 \
+        --port=8371 \
+        --model=Meta-Llama-3-70B-Instruct-AutoAWQ-4bit \
+        --tokenizer=meta-llama/Meta-Llama-3-70B-Instruct \
+        --tensor-parallel-size=1 \
+        --quantization awq \
+        --dtype half    \
+        --gpu-memory-utilization 0.9
